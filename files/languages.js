9a0cfa624ba48a7ba0d3b1e016604d4179e0cf86
CPK <CPKbaidu@163.com>
2025-03-19 19:14:11 +0800

    add es_ES, ru_RU, de_DE and ja_JP

--- files/languages.js
+++ files/languages.js
@@ -14,5 +14,37 @@ text = {
         'dk-speakup-petition-title': '签署请愿书',
         'dk-speakup-sign-button': '立刻签名',
         'dk-speakup-signature-text': '次签名'
+    },
+    'es_ES': {
+        'language-name': 'Español',
+        'entry-title': '¡Firma la petición!',
+        'entry-content-text': '<p>¡Hey, gente…!</p>\n<p>Vamos a ser honestos, a Nitrome no se le da muy bien hacer juegos. Recientemente anunciaron que van a sacar su juego número 100 y creo que es hora de decir "¡Basta ya!". Si no pudieron hacerlo bien con los primeros 99, ¡no van a sacar algo increíble de la manga ahora! Esta es mi petición para que Nitrome deje de hacer juegos y se lo deje a los pros.</p>\n<p>¡Firma la petición si estás de acuerdo!!</p>',
+        'dk-speakup-petition-title': 'FIRMA LA PETICIÓN',
+        'dk-speakup-sign-button': 'FIRMAR AHORA',
+        'dk-speakup-signature-text': 'firmas'
+    },
+    'ru_RU': {
+        'language-name': 'Русский',
+        'entry-title': 'Подпишите петицию!',
+        'entry-content-text': '<p>Всем привет…</p>\n<p>Давайте смотреть правде в глаза: игры от Nitrome — так себе. Недавно они заявили, что скоро выпустят свою 100-ю игру, и я считаю, что пора сказать: "Хватит, с нас достаточно!". Если они не смогли сделать это хорошо с первыми 99, они не смогут вдруг вытащить кролика из шляпы сейчас! Я запускаю эту петицию, чтобы попросить Nitrome завязать с играми и оставить это дело профи.</p>\n<p>Согласны? Подписывайте петицию!!</p>',
+        'dk-speakup-petition-title': 'ПОДПИСАТЬ ПЕТИЦИЮ',
+        'dk-speakup-sign-button': 'ПОДПИСАТЬ СЕЙЧАС',
+        'dk-speakup-signature-text': 'подписей'
+    },
+    'de_DE': {
+        'language-name': 'Deutsch',
+        'entry-title': 'Unterschreibe die Petition!',
+        'entry-content-text': '<p>Hey Leute…</p>\n<p>Seien wir ehrlich, Nitrome ist nicht so gut darin, Spiele zu machen. Sie haben kürzlich angekündigt, dass sie kurz davor sind, ihr 100. Spiel zu veröffentlichen, und ich denke, es ist an der Zeit zu sagen: "Genug ist genug!". Wenn sie es mit den ersten 99 nicht gut hinbekommen haben, werdet ihr jetzt nicht plötzlich ein Meisterwerk aus dem Ärmel schütteln! Dies ist meine Petition, um Nitrome zu bitten, mit dem Spielemachen aufzuhören und es den Profis zu überlassen.</p>\n<p>Unterschreibt die Petition, wenn ihr zustimmt!!</p>',
+        'dk-speakup-petition-title': 'UNTERSCHREIBE DIE PETITION',
+        'dk-speakup-sign-button': 'JETZT UNTERSCHREIBEN',
+        'dk-speakup-signature-text': 'Unterschriften'
+    },
+    'ja_JP': {
+        'language-name': '日本語',
+        'entry-title': '署名してね！',
+        'entry-content-text': '<p>ねえ、みんな…</p>\n<p>正直さ、Nitrome ってゲーム作るの下手くそだよね。最近、彼らが 100 作目のリリースに近づいてるって発表したけど、もう「いい加減にしろ！」って感じだよ。今まで 99 本もダメだったんだから、急にすごいのが出てくるわけないじゃん！だからさ、この請願で Nitrome にゲーム作りやめて、プロに任せてもらおうよ。</p>\n<p>賛成なら署名してね!!</p>',
+        'dk-speakup-petition-title': '請願書に署名',
+        'dk-speakup-sign-button': '今すぐ署名',
+        'dk-speakup-signature-text': '人署名'
     }
 }
